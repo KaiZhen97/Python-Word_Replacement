2cb55ebefbb2d753e0791fb37e5b498879a56b2e
KaiZhen97 <kaizhen970123@gmail.com>
2022-09-05 23:35:16 +0800

    Word_Replacement

---
+++ Word_Replacement.py
@@ -0,0 +1,10 @@
+def replace_word():
+    
+    str = "11 2 3 4 5 5"
+    word_to_replace = input("Word to replace ")
+    word_replacement = input("Word replacement ")
+    print(str.replace(word_to_replace, word_replacement))
+    
+replace_word()
+
+    
